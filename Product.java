3771455215902d8b302f3e9a8425934d55d8743f
unknown <y32kikas@hotmail.com>
2016-01-19 01:20:32 +0200

    Create class Product

---
+++ Product.java
@@ -0,0 +1,22 @@
+
+public abstract class Product {
+	
+	private String name;
+	private double price;
+	
+	public Product(String name, double price){
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	abstract public void paymentmethod(Payment p);
+
+}
